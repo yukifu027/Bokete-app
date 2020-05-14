6302db9e66dddaefa2f6b7ed5c851bf287acada5
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-05-14 14:20:42 +0900

    Initial Commit

---
+++ Swift5Bokete1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Bokete1
+//
+//  Created by 福井祐希 on 2020/05/14.
+//  Copyright © 2020 Yuki Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
